e945c94132645333c0c2ed07179d458291054a13
JonatasMSS <jonatas.miguelss@gmail.com>
2023-03-30 20:47:09 -0300

    feat(component):created a post item component to store post data and show it

--- src/pages/post_page/PostPage.tsx
+++ src/pages/post_page/PostPage.tsx
@@ -1,8 +1,8 @@
 import { useMediaQuery } from "react-responsive";
 import { Header } from "../../components/Header";
 import * as Tabs from '@radix-ui/react-tabs';
-import Like from '../../assets/icons/🦆 icon _heart_.svg';
-import Comment from '../../assets/icons/🦆 icon _message_.svg';
+import { PostItem } from "../../components/PostItem";
+
 export function PostPage() {
 
     const isSmallScreen = useMediaQuery({ query: '(max-width:640px)' })
@@ -32,45 +32,8 @@ export function PostPage() {
                             </Tabs.Trigger>
                         </Tabs.List>
                         <Tabs.Content value="em_alta" className="w-full flex flex-col py-5">
-                            <div className="w-full flex flex-col bg-DF-White rounded-lg font-K2D text-black p-2 ">
-
-                                {/*Post Header */}
-                                <div className="flex items-baseline md:gap-2">
-                                    {/* User and team */}
-                                    <div className="flex flex-col md:flex-row md:items-baseline md:gap-2">
-                                        <span className="font-bold text-sm md:text-base ">User's name</span>
-                                        <span className="font-medium text-sm">Equipe criadores</span>
-                                    </div>
-
-                                    {/* Time */}
-                                    <span className="text-sm text-zinc-700">Hora da postagem</span>
-
-                                </div>
-
-                                {/* Separator */}
-                                <div className="w-full my-2"></div>
-
-                                {/* Post Body */}
-                                <div className="w-full flex flex-col">
-                                    <span className="font-bold">Titulo do post</span>
-                                    <p className="text-sm font-light text-start break-words max-h-16 text-red-500 overflow-hidden">{Array.from({ length: 400 }).map(e => 'Lorem ipsum')}</p>
-                                </div>
-
-                                {/* Like Commment section */}
-                                <div className="w-full my-2 flex gap-2">
-                                    {/* Likes */}
-                                    <div className="flex gap-1">
-                                        <img src={Like} alt="Like" className="w-6" />
-                                        <span className="font-light">23</span>
-                                    </div>
-                                    {/* Commentarios */}
-                                    <div className="flex gap-1">
-                                        <img src={Comment} alt="Coment" className="w-6" />
-                                        <span className="font-light"> 44 comentários</span>
-                                    </div>
-                                </div>
-                            </div>
-
+                           
+                            <PostItem/>
 
 
                         </Tabs.Content>
