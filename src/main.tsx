c5407b8e39822a2eb4b479da2c47cc0c0e21ec13
JonatasMSS <jonatas.miguelss@gmail.com>
2023-04-08 13:41:17 -0300

    feat-refactor: added the lib dayjslocale and removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MainPage from './pages/MainPage';
 import { RouterProvider } from 'react-router';
 import routes from './routes/page.routes';
-
-
+import './lib/dayjs_locale.ts';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={routes} />
