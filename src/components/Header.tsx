921716506bb5fe6cfac741e3c68c0478044fc90a
JonatasMSS <jonatas.miguelss@gmail.com>
2023-04-11 00:49:54 -0300

    feat(style):added an transition color when hover in FCBrasil title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import { InlineUserData } from "./InlineUserData";
 export function Header(){
     return(
         <div className="w-full flex items-center justify-between bg-black font-K2D p-3 text-white">
-                <Link to={'/posts'} replace={true} >FC Brasil</Link>
+                <Link to={'/posts'} className="hover:underline hover:text-green-300 transition-all " replace={true} >FC Brasil</Link>
                 <InlineUserData/>
         </div>
     )
